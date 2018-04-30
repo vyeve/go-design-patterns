27f597599157a01d80e16c55be62cff531e1b074
XoBpaxa <xobpaxa@ukr.net>
2018-04-30 10:59:10 +0300

    added builder pattern

---
+++ creational/builder/car.go
@@ -0,0 +1,24 @@
+package creational
+
+type CarBuilder struct {
+	v VehicleProduct
+}
+
+func (cb *CarBuilder) SetWheels() BuildProcess {
+	cb.v.Wheels = 4
+	return cb
+}
+
+func (cb *CarBuilder) SetSeats() BuildProcess {
+	cb.v.Seats = 5
+	return cb
+}
+
+func (cb *CarBuilder) SetStructure() BuildProcess {
+	cb.v.Structure = "Car"
+	return cb
+}
+
+func (cb *CarBuilder) GetVehicle() VehicleProduct {
+	return cb.v
+}
