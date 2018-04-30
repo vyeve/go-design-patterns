0f5471580c0443044786bb8783cf2d7ed3e81310
XoBpaxa <xobpaxa@ukr.net>
2018-04-30 14:08:13 +0300

    added description to package

--- creational/abstract_factory/vehicle.go
+++ creational/abstract_factory/vehicle.go
@@ -1,3 +1,10 @@
+/*
+The Abstract Factory design pattern is a new layer of grouping to achieve a bigger composite
+object, which is used through its interfaces. The idea behind grouping objects in
+families and grouping families is to have big factories that can be interchangeable and can grow more
+easily.
+*/
+
 package abstract_factory
 
 type Vehicle interface {
