0aa962b401becf37423bf2c41f15148043a77af4
XoBpaxa <xobpaxa@ukr.net>
2018-04-30 12:43:35 +0300

    added factory method pattern

---
+++ creational/factory/factory.go
@@ -0,0 +1,35 @@
+/*
+The Factory method pattern abstracts the user from knowledge of the struct he needs
+to achieve for a specific purpose,such as retrieving some value, maybe from a web, service
+or a database. The user only needs an interface that provides him this value. By delegating
+this decision to a Factory, this Factory can provide an interface that fits the user needs.
+*/
+
+package creational
+
+import (
+	"fmt"
+)
+
+//PaymentMethod interface represents method Pay
+type PaymentMethod interface {
+	Pay(amount float32) string
+}
+
+// different types of payments
+const (
+	Cash = iota
+	DebitCard
+)
+
+// GetPaymentMethod return an object thay satisfies PaymentMethod interface
+func GetPaymentMethod(m int) (PaymentMethod, error) {
+	switch m {
+	case Cash:
+		return new(CashPM), nil
+	case DebitCard:
+		return new(DebitCardPM), nil
+	default:
+		return nil, fmt.Errorf("payment method with ID %d not recognized.\n", m)
+	}
+}
---
+++ creational/factory/factory_test.go
@@ -0,0 +1,40 @@
+package creational
+
+import (
+	"strings"
+	"testing"
+)
+
+func TestGetPaymentMethodCash(t *testing.T) {
+	payment, err := GetPaymentMethod(Cash)
+	if err != nil {
+		t.Fatal("a payment method of type 'Cash' must exist")
+	}
+	msg := payment.Pay(10.30)
+	if !strings.Contains(msg, "paid using cash") {
+		t.Error("the cash payment method wasn't correct")
+	}
+	t.Log("LOG:", msg)
+}
+
+func TestGetPaymentMethodDebitCard(t *testing.T) {
+	payment, err := GetPaymentMethod(DebitCard)
+	if err != nil {
+		t.Fatal("a payment method of type 'DebitCard' must exist")
+	}
+	msg := payment.Pay(22.30)
+	if !strings.Contains(msg, "paid using debit card") {
+		t.Error("the debit card payment method wasn't correct")
+	}
+	t.Log("LOG:", msg)
+}
+
+func TestGetPaymentMethodNonExistent(t *testing.T) {
+	id := 20
+	_, err := GetPaymentMethod(id)
+	if err == nil {
+		t.Errorf("a payment method with ID %d must return an error", id)
+	}
+	t.Logf("LOG: %v", err)
+
+}
